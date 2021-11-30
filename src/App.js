a5a72e02fa08e9d545540a877916677eef6521b5
shsv382 <shsv382@gmail.com>
2021-11-30 14:18:44 +0300

    Initial commit, all components created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Drakula',
+          id: 'asc2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3'
+        }
+      ],
+      searchField: ''
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await response.json()
+    this.setState({monsters: users})
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value}, () => console.log(this.state))
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    return (
+      <div className="App">
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }  
 }
 
 export default App;
